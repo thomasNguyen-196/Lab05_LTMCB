83d13eb8a34ae462a23020fa8e7f7c31b60acb2d
thomasNguyen-196 <thomasspielberg5@gmail.com>
2024-11-27 01:49:07 +0700

    Sửa được lỗi không kết nối được với MDaemon.

--- Lab05_LTMCB/Bai02.Designer.cs
+++ Lab05_LTMCB/Bai02.Designer.cs
@@ -28,76 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.total_tb = new System.Windows.Forms.TextBox();
+            this.recent_tb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.tb_pass = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.tb_mail = new System.Windows.Forms.TextBox();
-            this.pass_tb = new System.Windows.Forms.Label();
-            this.mail_tb = new System.Windows.Forms.Label();
+            this.pass_tb = new System.Windows.Forms.TextBox();
+            this.bt_login = new System.Windows.Forms.Button();
+            this.mail_tb = new System.Windows.Forms.TextBox();
+            this.pass_lbl = new System.Windows.Forms.Label();
+            this.mail_lbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // total_tb
+            // 
+            this.total_tb.Location = new System.Drawing.Point(131, 91);
+            this.total_tb.Name = "total_tb";
+            this.total_tb.Size = new System.Drawing.Size(100, 20);
+            this.total_tb.TabIndex = 20;
+            // 
+            // recent_tb
+            // 
+            this.recent_tb.Location = new System.Drawing.Point(338, 91);
+            this.recent_tb.Name = "recent_tb";
+            this.recent_tb.Size = new System.Drawing.Size(100, 20);
+            this.recent_tb.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Recent";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Total";
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(25, 124);
+            this.listView1.Location = new System.Drawing.Point(12, 125);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(753, 303);
-            this.listView1.TabIndex = 12;
+            this.listView1.Size = new System.Drawing.Size(776, 313);
+            this.listView1.TabIndex = 16;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // tb_pass
-            // 
-            this.tb_pass.Location = new System.Drawing.Point(98, 56);
-            this.tb_pass.Name = "tb_pass";
-            this.tb_pass.Size = new System.Drawing.Size(341, 20);
-            this.tb_pass.TabIndex = 11;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(639, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 49);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // tb_mail
-            // 
-            this.tb_mail.Location = new System.Drawing.Point(98, 23);
-            this.tb_mail.Name = "tb_mail";
-            this.tb_mail.Size = new System.Drawing.Size(341, 20);
-            this.tb_mail.TabIndex = 9;
             // 
             // pass_tb
             // 
-            this.pass_tb.AutoSize = true;
-            this.pass_tb.Location = new System.Drawing.Point(22, 59);
+            this.pass_tb.Location = new System.Drawing.Point(97, 45);
             this.pass_tb.Name = "pass_tb";
-            this.pass_tb.Size = new System.Drawing.Size(53, 13);
-            this.pass_tb.TabIndex = 8;
-            this.pass_tb.Text = "Password";
+            this.pass_tb.Size = new System.Drawing.Size(341, 20);
+            this.pass_tb.TabIndex = 15;
+            // 
+            // bt_login
+            // 
+            this.bt_login.Location = new System.Drawing.Point(638, 15);
+            this.bt_login.Name = "bt_login";
+            this.bt_login.Size = new System.Drawing.Size(122, 49);
+            this.bt_login.TabIndex = 14;
+            this.bt_login.Text = "LOGIN";
+            this.bt_login.UseVisualStyleBackColor = true;
+            this.bt_login.Click += new System.EventHandler(this.bt_login_Click);
             // 
             // mail_tb
             // 
-            this.mail_tb.AutoSize = true;
-            this.mail_tb.Location = new System.Drawing.Point(22, 26);
+            this.mail_tb.Location = new System.Drawing.Point(97, 12);
             this.mail_tb.Name = "mail_tb";
-            this.mail_tb.Size = new System.Drawing.Size(32, 13);
-            this.mail_tb.TabIndex = 7;
-            this.mail_tb.Text = "Email";
+            this.mail_tb.Size = new System.Drawing.Size(341, 20);
+            this.mail_tb.TabIndex = 13;
+            // 
+            // pass_lbl
+            // 
+            this.pass_lbl.AutoSize = true;
+            this.pass_lbl.Location = new System.Drawing.Point(21, 48);
+            this.pass_lbl.Name = "pass_lbl";
+            this.pass_lbl.Size = new System.Drawing.Size(53, 13);
+            this.pass_lbl.TabIndex = 12;
+            this.pass_lbl.Text = "Password";
+            // 
+            // mail_lbl
+            // 
+            this.mail_lbl.AutoSize = true;
+            this.mail_lbl.Location = new System.Drawing.Point(21, 15);
+            this.mail_lbl.Name = "mail_lbl";
+            this.mail_lbl.Size = new System.Drawing.Size(32, 13);
+            this.mail_lbl.TabIndex = 11;
+            this.mail_lbl.Text = "Email";
             // 
             // Bai02
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.total_tb);
+            this.Controls.Add(this.recent_tb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listView1);
-            this.Controls.Add(this.tb_pass);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.tb_mail);
             this.Controls.Add(this.pass_tb);
+            this.Controls.Add(this.bt_login);
             this.Controls.Add(this.mail_tb);
+            this.Controls.Add(this.pass_lbl);
+            this.Controls.Add(this.mail_lbl);
             this.Name = "Bai02";
             this.Text = "Bai02";
             this.ResumeLayout(false);
@@ -107,11 +147,15 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox total_tb;
+        private System.Windows.Forms.TextBox recent_tb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.TextBox tb_pass;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox tb_mail;
-        private System.Windows.Forms.Label pass_tb;
-        private System.Windows.Forms.Label mail_tb;
+        private System.Windows.Forms.TextBox pass_tb;
+        private System.Windows.Forms.Button bt_login;
+        private System.Windows.Forms.TextBox mail_tb;
+        private System.Windows.Forms.Label pass_lbl;
+        private System.Windows.Forms.Label mail_lbl;
     }
 }
--- Lab05_LTMCB/Bai02.cs
+++ Lab05_LTMCB/Bai02.cs
@@ -5,12 +5,14 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.Mail;
-using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MailKit;
 using MailKit.Net.Imap;
+using MailKit.Security;
+using MimeKit;
+using System.Net;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Lab05_LTMCB
@@ -22,53 +24,61 @@ namespace Lab05_LTMCB
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void bt_login_Click(object sender, EventArgs e)
         {
-
-            // Tạo client IMAP
-            using (var client = new ImapClient())
+            try
             {
-                // Kết nối đến server IMAP (ví dụ: Gmail sử dụng imap.gmail.com, cổng 993)
-                client.Connect("127.0.0.1", 993, true);
-
-                // Đăng nhập với thông tin từ giao diện
-                string email = tb_mail.Text;
-                string password = tb_pass.Text;
-                client.Authenticate(email, password);
-
-                // Truy cập hộp thư "INBOX"
-                var inbox = client.Inbox;
-                inbox.Open(FolderAccess.ReadOnly);
-
-                // Cấu hình ListView
-                listView1.Columns.Add("Email", 200);
-                listView1.Columns.Add("From", 100);
-                listView1.Columns.Add("Thời gian", 100);
-                listView1.View = View.Details;
-
-                // Lấy danh sách email
-                for (int i = 0; i < inbox.Count; i++)
+                // Tạo client IMAP
+                using (var client = new ImapClient())
                 {
-                    var message = inbox.GetMessage(i);
-                    ListViewItem name = new ListViewItem(message.Subject);
+                    // Kết nối đến server IMAP (ví dụ: Gmail sử dụng imap.gmail.com, cổng 993)
+                    client.Connect("127.0.0.1", 143, SecureSocketOptions.None);
 
-                    // Thêm các thông tin vào cột
-                    ListViewItem.ListViewSubItem from = new ListViewItem.ListViewSubItem(name, message.From.ToString());
-                    ListViewItem.ListViewSubItem date = new ListViewItem.ListViewSubItem(name, message.Date.Date.ToString());
+                    // Đăng nhập với thông tin từ giao diện
+                    string email = mail_tb.Text.ToString().Trim();
+                    string password = pass_tb.Text.ToString().Trim();
+                    client.Authenticate(email, password);
 
-                    name.SubItems.Add(from);
-                    name.SubItems.Add(date);
+                    // Truy cập hộp thư "INBOX"
+                    var inbox = client.Inbox;
+                    inbox.Open(FolderAccess.ReadOnly);
 
-                    listView1.Items.Add(name);
+                    // Cấu hình ListView
+                    listView1.Columns.Add("Email", 200);
+                    listView1.Columns.Add("From", 100);
+                    listView1.Columns.Add("Thời gian", 100);
+                    listView1.View = View.Details;
+
+                    // Lấy danh sách email
+                    for (int i = 0; i < inbox.Count; i++)
+                    {
+                        var message = inbox.GetMessage(i);
+                        ListViewItem name = new ListViewItem(message.Subject);
+
+                        // Thêm các thông tin vào cột
+                        ListViewItem.ListViewSubItem from = new ListViewItem.ListViewSubItem(name, message.From.ToString());
+                        ListViewItem.ListViewSubItem date = new ListViewItem.ListViewSubItem(name, message.Date.Date.ToString());
+
+                        name.SubItems.Add(from);
+                        name.SubItems.Add(date);
+
+                        listView1.Items.Add(name);
+                        total_tb.Text = inbox.Count.ToString();
+                        recent_tb.Text = inbox.Recent.ToString();
+                    }
+
+                    // Ngắt kết nối
+                    client.Disconnect(true);
+
+
+                    MessageBox.Show("Đăng nhập và tải email thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
-
-                // Ngắt kết nối
-                client.Disconnect(true);
-
-                MessageBox.Show("Đăng nhập và tải email thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-
+            catch (Exception ex)
+            {
+                // Xử lý lỗi
+                MessageBox.Show($"Đã xảy ra lỗi: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
-
     }
 }
